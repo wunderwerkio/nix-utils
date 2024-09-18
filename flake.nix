62f9a98dd305d26670be07aeff530399863e69fd
Christian Foidl <christian@wunderwerk.io>
2024-09-18 14:27:32 +0200

    feat: add devenv-helper script

--- flake.nix
+++ flake.nix
@@ -15,14 +15,24 @@
       "x86_64-darwin"
       "aarch64-darwin"
     ];
+
+    # Function that generates an attribute set for each
+    # system that the wunderwerk team uses for development.
+    # See: https://github.com/NixOS/nixpkgs/blob/f64b9738da8e86195766147e9752c67fccee006c/lib/attrsets.nix#L613
+    forEachWunderwerkSystem = nixpkgs.lib.genAttrs systems;
   in {
     lib = {
-      # Function that generates an attribute set for each
-      # system that the wunderwerk team uses for development.
-      # See: https://github.com/NixOS/nixpkgs/blob/f64b9738da8e86195766147e9752c67fccee006c/lib/attrsets.nix#L613
-      forEachWunderwerkSystem = nixpkgs.lib.genAttrs systems;
+      inherit forEachWunderwerkSystem;
     };
 
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    packages = forEachWunderwerkSystem(system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devenv-helper = import ./nix/devenv-helper.nix {
+        inherit pkgs;
+      };
+    });
   };
 }
